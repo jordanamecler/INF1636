647b829ba27a1445f77053f03cb5122e93d6d1e5
jordanamecler <jordana.mecler@hotmail.com>
2016-11-04 18:58:28 -0200

    Testando uns desenhos com Graphics2D

--- Trabalho/1311354/src/PosicionarFrame.java
+++ Trabalho/1311354/src/PosicionarFrame.java
@@ -1,18 +1,28 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class PosicionarFrame extends JFrame
 {
-	public final int LARG_DEFAULT = 900;
-	public final int ALT_DEFAULT = 600;
+	private final int LARG_DEFAULT = 900;
+	private final int ALT_DEFAULT = 600;
 	
 	public PosicionarFrame ()
 	{
-		setLayout (null);
-		setTitle ("Batalha Naval");
+		Toolkit tk = Toolkit.getDefaultToolkit ();
+		Dimension screenSize = tk.getScreenSize ();
+		int sl = screenSize.width;
+		int sa = screenSize.height;
+		int x = sl/2 - LARG_DEFAULT/2;
+		int y = sa/2 - ALT_DEFAULT/2;
 		
-		System.out.println(InformacoesGlobais.getInformacoesGlobais().getJogador(1));
+		setBounds (x, y, LARG_DEFAULT, ALT_DEFAULT);
 		
 		setSize (LARG_DEFAULT, ALT_DEFAULT);
 		setDefaultCloseOperation (EXIT_ON_CLOSE);
+		
+		getContentPane ().add (new Panel ());
+		
+		System.out.println(InformacoesGlobais.getInformacoesGlobais().getJogador(1));
+		
 	}
 }
